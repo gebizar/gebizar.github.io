b8db14c6cd64cdf3e54714dc9d4a4daae9913276
Gabe <gebizar@uw.edu>
2019-02-05 19:59:05 -0800

    Updates

---
+++ precache-manifest.ea55dac81113eb062ef51c65c6f71a0c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ed41ea1a0c116fa97fad1e36ba0daba1",
+    "url": "/team2/static/media/Dave.ed41ea1a.jpg"
+  },
+  {
+    "revision": "fad4415f87483e356f97",
+    "url": "/team2/static/css/main.600ccd4f.chunk.css"
+  },
+  {
+    "revision": "3496e7a065857d219bb3",
+    "url": "/team2/static/js/1.3496e7a0.chunk.js"
+  },
+  {
+    "revision": "1adc758c63cb9d14f51e",
+    "url": "/team2/static/js/runtime~main.1adc758c.js"
+  },
+  {
+    "revision": "955fae3e2d3b6d48c1a4c53630e8a797",
+    "url": "/team2/static/media/Alex.955fae3e.jpg"
+  },
+  {
+    "revision": "fad4415f87483e356f97",
+    "url": "/team2/static/js/main.fad4415f.chunk.js"
+  },
+  {
+    "revision": "cbbeeae0eb21afd7d2459c2ceadc897f",
+    "url": "/team2/static/media/Gabe.cbbeeae0.jpg"
+  },
+  {
+    "revision": "f742ed442a7fa23ab57d79dbea92892c",
+    "url": "/team2/static/media/Tu.f742ed44.jpg"
+  },
+  {
+    "revision": "4c50563a3e9068108767f306bce16b6f",
+    "url": "/team2/static/media/pizza.4c50563a.jpeg"
+  },
+  {
+    "revision": "7196d18b5b5a29d987eac975a0ac19d7",
+    "url": "/team2/static/media/pizza_icon.7196d18b.png"
+  },
+  {
+    "revision": "f75e9124a14c16344d4958349111060f",
+    "url": "/team2/index.html"
+  }
+];
